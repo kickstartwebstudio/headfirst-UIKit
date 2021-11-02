b86c85f501fb2be5e7fd7fde98931d948ed04c9f
Wardhana Rizaldi <40145705+wardhanarizaldi@users.noreply.github.com>
2021-11-02 21:01:42 +0700

    UPDATE

--- inc-header.php
+++ inc-header.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Headfirst</title>
 <meta name="description" content="Starter kit to develop with UIKit CSS Framework. Please modify to suit your needs">
-<link rel="icon" href="assets/img/favicon.ico">
+<!-- <link rel="icon" href="assets/img/favicon.ico"> -->
 
 <!-- OG -->
 <meta property="og:title" content="Headfirst" />
--- index.php
+++ index.php
@@ -1,16 +1,61 @@
 <?php include 'inc-header.php';?>
 
-<section class="uk-cover-container" uk-height-viewport>
-    <img src="https://source.unsplash.com/ddJ9PNYjric/1920x1080" alt="" uk-cover>
-            <div class="uk-overlay-primary uk-position-cover"></div>
-            <div class="uk-overlay uk-position-center uk-light">
-                <div class="uk-container">
-                    <h1>Let's Go Digital</h1>
-                </div>
-                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>    
-</section>
+<!-- TOP -->
+<div class="top-wrap uk-position-relative uk-light uk-background-secondary">
 
+    <!-- NAV -->
+    <div class="nav" data-uk-sticky="cls-active: uk-background-secondary uk-box-shadow-medium; top: 100vh; animation: uk-animation-slide-top">
+        <div class="uk-container">
+            <nav class="" uk-navbar>
+                <div class="uk-navbar-left">
+                    <div class="uk-text-bold uk-logo">.headfirst</div>
+                </div>
+                <div class="uk-navbar-right">
+                    <ul class="uk-navbar-nav uk-visible@s">
+                        <?php include 'inc-navbar-menu.php';?>
+                    </ul>
+                  <a class="uk-navbar-toggle uk-navbar-item uk-hidden@s" data-uk-toggle data-uk-navbar-toggle-icon href="#offcanvas-nav"></a>
+                </div>
+            </nav>           
+        </div>
+
+
+    </div>
+    <!-- /NAV -->
+
+    <div class="uk-cover-container uk-light uk-flex uk-flex-middle" uk-height-viewport>
+
+        <!-- TOP CONTAINER -->
+        <div class="uk-container uk-flex-auto top-container uk-position-relative uk-margin-medium-top" data-uk-parallax="y: 0,50; easing:0; opacity:0.2">
+            <div class="uk-width-1-2@s" data-uk-scrollspy="cls: uk-animation-slide-right-medium; target: > *; delay: 150">
+                <h6 class="uk-text-primary uk-margin-small-bottom">RESEARCH</h6>
+                <h1 class="uk-margin-remove-top">Innovation in your hands.</h1>
+                <p class="subtitle-text uk-visible@s">Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco </p>
+                <a href="#" title="Learn More" class="uk-button uk-button-primary uk-border-pill" data-uk-scrollspy-class="uk-animation-fade">LEARN MORE</a>
+            </div>
+        </div>
+        <!-- /TOP CONTAINER -->
+        <!-- TOP IMAGE -->
+        <img src="https://source.unsplash.com//1920x1080" alt="" data-uk-cover data-uk-img data-uk-parallax="opacity: 1,0.1; easing:0">
+        <!-- /TOP IMAGE -->
+    </div>
+    <div class="uk-position-bottom-center uk-position-medium uk-position-z-index uk-text-center">
+        <a href="#content" data-uk-scroll="duration: 500" data-uk-icon="icon: arrow-down; ratio: 2"></a>
+    </div>
+</div>
+<!-- /TOP -->
+
+<section class="uk-section">
+    <div class="uk-container">
+        <h2>Heading</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nam accusantium tempora labore, et quo quidem eveniet illo suscipit laudantium ullam distinctio similique eum ducimus. Eum veniam quos quo aliquid sunt distinctio accusamus assumenda, ab reiciendis, minus architecto enim dolores doloribus dicta, dignissimos vitae debitis laudantium praesentium! Dolorum nulla amet vel eveniet molestiae sunt et, suscipit sint quod necessitatibus aperiam. Blanditiis incidunt iure dignissimos quod laudantium eligendi ex perspiciatis! Voluptatibus, numquam. Illum dignissimos iure ex tenetur eum, nostrum, debitis sunt minima nesciunt eius. Esse explicabo quas rem eius unde perferendis molestiae voluptate. Fugit libero quo aliquam nisi maiores debitis vero animi quia quod nulla, impedit quam assumenda repellat suscipit corrupti a dignissimos, sequi possimus. Enim quidem, placeat vitae minima dolor fuga animi, voluptatum harum eos quae ipsum a? Inventore, autem illo, earum voluptatibus eveniet facere unde harum iusto saepe modi sapiente esse, doloremque explicabo, amet cumque optio labore vero dolorum quidem natus perferendis maxime. Labore, officiis. Ipsum harum deserunt id possimus ex qui aliquam labore, ut ipsam hic soluta eos, fugit culpa aspernatur suscipit a voluptas itaque porro dignissimos voluptatibus fugiat maxime vero ab quasi? Distinctio, adipisci, eligendi! Excepturi, expedita ad exercitationem eius hic minus. Consequuntur fuga hic, cumque ratione neque eius recusandae rem tenetur saepe, tempora excepturi facilis iusto esse dolores quasi omnis rerum doloremque quam expedita provident, natus voluptas assumenda odit consequatur? Aut nemo eos porro accusamus atque minima impedit, qui voluptatibus recusandae amet similique iste iure ex, odit illum, totam praesentium aspernatur. Esse sapiente dolor neque, inventore ipsum quae amet reprehenderit quia incidunt iure voluptates! Quod maxime nemo incidunt, corporis repellendus quia. Aliquam quaerat, ad maiores, eveniet mollitia quis? Delectus dolore, adipisci voluptates quae ad iusto. Eligendi ducimus nobis maxime consequuntur, aliquam neque impedit sapiente perferendis quia repudiandae iure vero suscipit nihil veniam eos voluptatem porro beatae hic nostrum dolores natus modi qui? Mollitia sit, aut animi enim atque vero aliquam autem nobis fugiat a vel provident! Numquam perspiciatis ipsam doloremque tempora provident blanditiis tempore optio tenetur ipsa vero nihil dignissimos nobis a enim, officia fugiat odio voluptas recusandae in quas. Aperiam nulla excepturi nemo natus fuga aliquid provident voluptatem sapiente, rerum impedit sint eveniet ratione nam quam, voluptatibus expedita molestiae illum incidunt sunt atque omnis blanditiis, quibusdam quia. Maxime itaque unde quae. Aliquam laudantium blanditiis cum, nobis quaerat, omnis. Tempora a, expedita illo facilis possimus unde magnam omnis, cupiditate in reprehenderit sunt reiciendis consequatur delectus ducimus excepturi inventore aperiam dolores adipisci culpa ea eaque nisi dolor. Corporis sed voluptates eius, asperiores voluptatum, pariatur dolor tempora aspernatur est, veritatis vero sunt autem doloribus quasi ad sit? Autem amet soluta modi illum animi eveniet placeat impedit porro odio quod beatae suscipit deleniti, omnis vel minus asperiores blanditiis natus nemo officiis? Autem dignissimos quas explicabo sit, hic pariatur ullam laborum inventore voluptatibus corrupti aliquam fugiat iure, qui nemo minima debitis, dolor quod dolorem nihil. Veritatis reprehenderit quas nemo nam iusto, iure doloribus sit! Iste, adipisci et dicta voluptatibus tempore earum pariatur ipsam facilis neque eos autem similique facere dolore!</p>
+    </div>
+</section>
+   
+<section class="uk-section">
+    <div class="uk-container"><img src="https://source.unsplash.com/RNfrfBDxh1k/1600x900" alt=""></div>
+</section>
     
 
 		
